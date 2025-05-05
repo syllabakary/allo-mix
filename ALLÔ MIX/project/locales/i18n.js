be8e9a1a949b0b90407a8996bab055f0c4f4734d
sylla bakary <bakary.sylla@epitech.eu>
2025-05-05 20:23:02 +0100

    Ajout des traductions FR avec i18n

---
+++ ALLÔ MIX/project/locales/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import fr from './locales/fr.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    compatibilityJSON: 'v3',
+    lng: 'fr',
+    fallbackLng: 'fr',
+    resources: {
+      fr: {
+        translation: fr
+      }
+    },
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
